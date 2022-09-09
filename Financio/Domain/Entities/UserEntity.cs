4e0fb966b632ae3635ec313670b4d1d91c227e8c
pbesra <psbesra@gmail.com>
2022-09-09 13:02:58 +0530

    Initial commit

---
+++ Financio/Domain/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Domain.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsDisabled { get; set; }
+    }
+}
